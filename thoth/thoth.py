4a3f7e495f77b50529b809c7a6ea471dae24d5de
marcomaza92 <marcoagustinmaza@gmail.com>
2024-07-03 23:38:55 -0300

    fix: update line-length to less than 100

--- thoth/thoth.py
+++ thoth/thoth.py
@@ -29,15 +29,34 @@ def index() -> rx.Component:
         rx.container(
             rx.heading("Branching", size="9"),
             rx.text(
-                    "Tomamos la decisión de hacer un Branching con dos ramas: una de desarrollo (Develop) y otra de producción (Main) debido a que planteamos el caso en el que fuéramos desarrolladores de un equipo para una empresa chica. Entonces, nos pareció la mejor idea usar dos entornos de trabajo para reducir costos y porque veíamos que era innecesario que fuera más complejo. Decidimos combinar GitFlow y GitLab Flow por las siguientes razones: poder separar en ramas de Desarrollo y Produccion, y ademas nos da la opcion de poder utilizar otras ramas como Feature para el desarrollo de nuevas caracteristicas,Realese y Hotfix para correciones rapidas y controlar las versiones del codigo, ya que el uso de ramas dedicadas nos da un historial mas organizado.",
+                "Tomamos la decisión de hacer un Branching con dos ramas: una de \
+                desarrollo (Develop) y otra de producción (Main) debido a que \
+                planteamos el caso en el que fuéramos desarrolladores de un equipo \
+                para una empresa chica. Entonces, nos pareció la mejor idea usar dos \
+                entornos de trabajo para reducir costos y porque veíamos que era \
+                innecesario que fuera más complejo. Decidimos combinar GitFlow y \
+                GitLab Flow por las siguientes razones: poder separar en ramas de \
+                Desarrollo y Produccion, y ademas nos da la opcion de poder utilizar \
+                otras ramas como Feature para el desarrollo de nuevas caracteristicas, \
+                Release y Hotfix para correciones rapidas y controlar las versiones del codigo, \
+                ya que el uso de ramas dedicadas nos da un historial mas organizado.",
             ),
             rx.text(
-                    "Y para realizar los commits de las Branches decidimos utilizar Conventional Commits para mejorar la automatizacion y claridad debido a que utiliza distintios tipos como:",
+                "Y para realizar los commits de las Branches decidimos utilizar \
+                Conventional Commits para mejorar la automatizacion y claridad \
+                debido a que utiliza distintios tipos como:",
             ),
             rx.list.unordered(
-                rx.list.item("Feat= agrega funcionalidades parciales o completas"),
-                rx.list.item("Fix= arregla errores que impiden el correcto funcionamiento de una feature"),
-                rx.list.item("Docs= modifica archivos de documentación o archivos de código que requieran documentación"),
+                rx.list.item(
+                    "Feat= agrega funcionalidades parciales o completas"
+                ),
+                rx.list.item(
+                    "Fix= arregla errores que impiden el correcto funcionamiento de una feature"
+                ),
+                rx.list.item(
+                    "Docs= modifica archivos de documentación o archivos de código \
+                    que requieran documentación"
+                ),
                 rx.list.item("Test"),
                 rx.list.item("etc"),
             ),
@@ -46,33 +65,30 @@ def index() -> rx.Component:
             min_height="50vh",
         ),
         rx.container(
-          rx.color_mode.button(position="top-right"),
-          rx.vstack(
-            rx.heading("Commit", size="9"),
-            rx.text(
-              "Conventional Commit",
-              size="5",
+            rx.color_mode.button(position="top-right"),
+            rx.vstack(
+                rx.heading("Commit", size="9"),
+                rx.text(
+                    "Conventional Commit",
+                    size="5",
+                ),
+                rx.list.unordered(
+                    rx.list.item("scope: message"),
+                    rx.list.item(
+                        "feat: add new container for the Branching information"
+                    ),
+                    rx.list.item("fix: update app setup in README.md"),
+                    rx.list.item("chore: remove unused packages"),
+                ),
+                spacing="5",
+                justify="center",
+                min_height="85vh",
             ),
-            rx.list.unordered(
-              rx.list.item("scope: message"),
-              rx.list.item("feat: add new container for the Branching information"),
-              rx.list.item("fix: update app setup in README.md"),
-              rx.list.item("chore: remove unused packages")
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-          ),
-          
         ),
         spacing="5",
         justify="center",
-        min_height="85vh"
+        min_height="85vh",
     )
-    
-  
-  
-
 
 
 app = rx.App()  # pylint: disable=not-callable
