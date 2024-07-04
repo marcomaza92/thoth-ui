8362c66f864554ba3e9d35f86c0ed06a59d1b2e9
TomasRomeu <tomashhromeu@gmail.com>
2024-07-03 21:45:45 -0300

    fix: add a new list and  paragraph

--- thoth/thoth.py
+++ thoth/thoth.py
@@ -29,9 +29,18 @@ def index() -> rx.Component:
         rx.container(
             rx.heading("Branching", size="9"),
             rx.text(
-                    "Tomamos la decisión de hacer un Branching con dos ramas: una de desarrollo (Develop) y otra de producción (Main) debido a que planteamos el caso en el que fuéramos desarrolladores de un equipo para una empresa chica. Entonces, nos pareció la mejor idea usar dos entornos de trabajo para reducir costos y porque veíamos que era innecesario que fuera más complejo. Decidimos combinar GitFlow y GitLab Flow por las siguientes razones: poder separar en ramas de Desarrollo y Produccion, y ademas nos da la opcion de poder utilizar otras ramas como Feature para el desarrollo de nuevas caracteristicas,Realese y Hotfix para correciones rapidas y controlar las versiones del codigo, ya que el uso de ramas dedicadas nos da un historial mas organizado. Y para realizar los commits de las Branches decidimos utilizar Conventional Commits para mejorar la automatizacion y claridad debido a que utiliza distintios tipos como (Feat, Fix, Docs, Test)",
+                    "Tomamos la decisión de hacer un Branching con dos ramas: una de desarrollo (Develop) y otra de producción (Main) debido a que planteamos el caso en el que fuéramos desarrolladores de un equipo para una empresa chica. Entonces, nos pareció la mejor idea usar dos entornos de trabajo para reducir costos y porque veíamos que era innecesario que fuera más complejo. Decidimos combinar GitFlow y GitLab Flow por las siguientes razones: poder separar en ramas de Desarrollo y Produccion, y ademas nos da la opcion de poder utilizar otras ramas como Feature para el desarrollo de nuevas caracteristicas,Realese y Hotfix para correciones rapidas y controlar las versiones del codigo, ya que el uso de ramas dedicadas nos da un historial mas organizado.",
             ),
-            rx.button("Click Me!", on_click = print("Button Clicked")),
+            rx.text(
+                    "Y para realizar los commits de las Branches decidimos utilizar Conventional Commits para mejorar la automatizacion y claridad debido a que utiliza distintios tipos como:",
+            ),
+            rx.list.unordered(
+                rx.list.item("Feat= agrega funcionalidades parciales o completas"),
+                rx.list.item("Fix= arregla errores que impiden el correcto funcionamiento de una feature"),
+                rx.list.item("Docs= modifica archivos de documentación o archivos de código que requieran documentación"),
+                rx.list.item("Test"),
+                rx.list.item("etc"),
+            ),
             spacing="5",
             justify="center",
             min_height="50vh",
@@ -53,7 +62,7 @@ def index() -> rx.Component:
                 min_height="85vh",
             ),
         )
-    
+
 
 
 app = rx.App()  # pylint: disable=not-callable
