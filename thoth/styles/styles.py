ea8047a63ac0c44d3adb744526c334c3ae43ac40
marcomaza92 <marcoagustinmaza@gmail.com>
2024-07-05 00:41:25 -0300

    feat: isolate styles and labels

---
+++ thoth/styles/styles.py
@@ -0,0 +1,15 @@
+primary_color = {
+    "color": "#51c293",
+}
+text_style = {
+"font-size": "1rem",
+    
+}
+h1_style = {
+    "font_size": "3rem",
+    "padding_bottom": "1.5rem"
+}
+h2_style = {
+    "font_size": "2rem",
+    "padding_bottom": "1rem"
+}
