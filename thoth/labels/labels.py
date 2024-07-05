e85521c54ba36d83296f81d4617eec1582a7d050
marcomaza92 <marcoagustinmaza@gmail.com>
2024-07-05 10:47:34 -0300

    docs: add ci/cd section

--- thoth/labels/labels.py
+++ thoth/labels/labels.py
@@ -30,7 +30,7 @@ branching = {
     "content_ii": "Y para realizar los commits de las Branches decidimos utilizar \
     Conventional Commits para mejorar la automatizacion y claridad \
     debido a que utiliza distintios tipos como:",
-    "list": [
+    "list_i": [
         "Feat= agrega funcionalidades parciales o completas",
         "Fix= arregla errores que impiden el correcto funcionamiento de una feature",
         "Docs= modifica archivos de documentación o archivos de código \
@@ -41,7 +41,7 @@ branching = {
 commits = {
     "title": "Commits",
     "content_i": "Conventional Commit",
-    "list": [
+    "list_i": [
         "scope: message",
         "feat: add new container for the Branching information",
         "fix: update app setup in README.md",
@@ -58,6 +58,7 @@ analysis = {
     Luego, para formatear parte de ese código, utilizamos Black que está \
     basado en autopep8 (que sigue la convencieon PEP 8) con la diferencia que \
     es más opinionado, lo cual facilita el convention over configuration.",
+    "image_i": "/analysis_i.png"
 }
 
 code_review = {
@@ -66,11 +67,37 @@ code_review = {
   implementamos un proceso de Code Review para nuestras MR (Merge Requests).",
     "content_ii": "Entre los puntos clave del manejo de MRs y code review, \
   podemos mencionar los siguientes:",
-    "list": [
+    "list_i": [
         "Inclusión de un MR Template para homogenizar la estructura de las mismas",
         "Al menos un approval de un peer",
         "Inclusión de pipelines de análisis de código y build de la aplicación"
         "Uso de thread para mejor organización de los cambios requeridos",
     ],
-    "link": "https://gitlab.com/marcomaza92/thoth/-/merge_requests/2",
+    "link_i": "https://gitlab.com/marcomaza92/thoth/-/merge_requests/2",
+}
+
+ci_cd = {
+  "title": "CI/CD",
+  "content_i":
+    "En la etapa de CI/CD, tratamos de hacer un flujo simple pero \
+    completo, o al menos que cubra de manera ilustrativa los 3 stages más comunes \
+    que son:",
+  "list_i": [
+    "test",
+    "build",
+    "deploy"
+  ],
+  "content_ii": "Podemos ver esto reflejado en el archivo yaml:",
+  "image_i": "/ci_cd_i.png",
+  "content_iii":
+    "Con esto presente, en cada stage pusimos a correr ciertas tareas \
+    para cada job. Podemos resumirlas de la siguiente forma:",
+"list_ii": [
+  "test: ejecutamos el formatter y luego el linter",
+  "build: corremos un comando para obtener artifacts estáticos para deployear",
+  "deploy: usando GitLab Pages, exponemos los artifacts para que sean visibles \
+    de manera pública a través de una URL"
+],
+"image_ii": "/ci_cd_ii.png",
+
 }
